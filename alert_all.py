55eb237189440434b372e2d49c40d70646921a9c
YX <yu_xuan_tan@hotmail.com>
2024-10-17 15:42:24 +0800

    fix alerts

--- alert_all.py
+++ alert_all.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import utils.telegram_controller as tc
 import utils.supabase as db
 import utils.ticker_getter as tg
@@ -23,7 +23,7 @@ def alert(indicator_name):
         if three_weekdays_ago.weekday() < 5:  # Monday to Friday are 0-4
             weekdays_count += 1
 
-    results_output = "__ *ticker | entry date | next earning report * __\n" 
+    results_output = "__ *ticker | entry date | next earning report* __\n" 
 
     # Filter and format the data
     filtered_tickers = []
@@ -31,44 +31,53 @@ def alert(indicator_name):
         # get last date in ticker["dates"]
         if not row['dates']:
             continue
-        entry_date = datetime.strptime(row['dates'][-1], '%Y-%m-%d')
+        signal_date = datetime.strptime(row['dates'][-1], '%Y-%m-%d')
         # Calculate the entry date as 2 weekdays away from signal confirmed bar date because we are using 2D chart. 
-
+        entry_date = signal_date
         days_added = 0
         while days_added < 2:
             entry_date += timedelta(days=1)
             if entry_date.weekday() < 5:  # Monday to Friday are 0-4
                 days_added += 1
             
+
         if entry_date > three_weekdays_ago:
             filtered_tickers.append(row)
             ticker_symbol = row["ticker"]
             data = tg.fetch_stock_data(ticker_symbol)
-            entry_close_price = data.loc[entry_date].get('Close', '?')
-            volume = data.loc[entry_date].get('Volume', '?')
+
+            latest_close = data.iloc[-1].get('Close', '?')
+            latest_volume = data.iloc[-1].get('Volume', '?')
 
             # Format entry close price and volume
-            if entry_close_price != '?':
-                entry_close_price = round(float(entry_close_price), 2)
+            if latest_close != '?':
+                latest_close = round(float(latest_close), 2)
 
-            if volume != '?':
-                volume = round(float(volume))
+            if latest_volume != '?':
+                latest_volume = round(float(latest_volume))
 
-            if entry_close_price == '?' or volume == '?' or entry_close_price < 20 or volume < 1000000:
+            if latest_close == '?' or latest_volume == '?' or latest_close < 20 or latest_volume < 1000000:
                 continue
+
+            next_earnings_date = tg.fetch_next_earnings_date(ticker_symbol)
+            if next_earnings_date is None or next_earnings_date > (datetime.now(timezone.utc) + timedelta(days=14)):
+                continue
+
 
             # Add row to the table
             results_output += f"✅ *{ticker_symbol}* | {str(entry_date).split(' ')[0]} | {str(tg.fetch_next_earnings_date(ticker_symbol)).split(' ')[0]}\n"
 
+    msg = f"""*{indicator_name} screening completed on {datetime.now().date()}*\n
+*Filter ⚙️:*\n ‣ Entry date up to 2 weekdays ago \n ‣ Latest close price > 20 \n ‣ Latest volume > 100k \n ‣ Earnings Report in next 14 days\n\n"""
     # Check if there are results
     if not filtered_tickers:
-        return f"*{indicator_name} screening completed*\n\n⚙️ Close price > 20, Volume > 100k\n\nNo stocks found matching the criteria"
+        msg += "No stocks found matching the criteria."
+    else:
+        msg += f"*Screen results:*\n{results_output}"
 
-    # Use PrettyTable's string representation to create the message
 
-    output_msg = f"""*{indicator_name} screening completed*\n\n⚙️ Close price > 20, Volume > 100k \n\n *Screen results:*\n{results_output}"""
     
-    return output_msg
+    return msg
 
 
 if __name__ == "__main__":
