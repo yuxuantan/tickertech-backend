4c8815b1a0dd09f2f0e7f0f5fe05e1d091d2b16d
YX <yu_xuan_tan@hotmail.com>
2024-10-20 10:35:19 +0800

    max out output

--- entry_screener.py
+++ entry_screener.py
@@ -12,7 +12,7 @@ symbol = "BTC/USD"
 gap_for_sl = 1.88
 
 interval = 15  # Can be 1min, 5min, 15min, 30min, 1h, etc.
-output_size = 1000
+output_size = 5000
 
 sl_per_trade = 20
 risk_reward_ratio = 2
